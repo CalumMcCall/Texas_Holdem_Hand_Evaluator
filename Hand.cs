afbc727fb0a8cceaabd840cfd8f169008e49d85c
Calum McCall <mccallcalum@gmail.com>
2013-08-20 20:45:31 +0100

    Implemented HasStraightFlush() and HasFlush(). Added stub methods for all planned hand checking methods in Hand class. Added Unit Tests for all hand checking methods in the Hand class. Ordered Unit Tests and methods in Hand class according to Hand Ranking.

--- Hand.cs
+++ Hand.cs
@@ -22,6 +22,95 @@ namespace TexasHoldemHandEvaluator
 			}
 		}
 
+		private Hand (List<Card> cs)
+		{
+			cards = new List<Card> ();
+			foreach(Card c in cs)
+			{
+				cards.Add (c.DeepCopy());
+			}
+		}
+
+		public Card HasStraightFlush()
+		{
+			Card highest = HasFlush();
+			if(highest == null) {
+				return null;
+			}
+
+			var flushCards = new List<Card>();
+			foreach(Card c in cards) {
+				if(c.suit == highest.suit) {
+					flushCards.Add(c);
+				}
+			}
+			Hand newHand = new Hand(flushCards);
+			String card = newHand.HasStraight();
+			if(card == ""){
+				return null;
+			}
+
+			try {
+				Value handValue = (Value) Enum.Parse(typeof(Value), card);
+				if(Enum.IsDefined(typeof(Value), handValue)) {
+					return new Card(highest.suit, handValue);
+				}
+				return null;
+			} catch (ArgumentException) {
+				Console.Error.WriteLine("HasStraight() returned an invalid card Value!");
+				return null;
+			}
+		}
+		
+		/* Returns the Card value of the quads on the board, or 
+		 * an empty string if there are no quads */
+		public String HasQuads()
+		{
+			var counts = new Dictionary<Value, int>();
+			foreach(Card c in cards) {
+				if(counts.ContainsKey(c.value)) {
+					counts[c.value] += 1;
+				} else {
+					counts.Add(c.value, 1);
+				}
+			}
+			foreach(KeyValuePair<Value, int> kvp in counts) {
+				if(kvp.Value == 4) {
+					return kvp.Key.ToString();
+				}
+			}
+
+			return "";
+		}
+
+		/* Checks for a flush and if found, returns the highest card
+		 * in the flush, else null */
+		public Card HasFlush()
+		{
+			var counts = new Dictionary<Suit, int> ();
+			foreach (Card c in cards) {
+				if (counts.ContainsKey (c.suit)) {
+					counts [c.suit] += 1;
+				} else {
+					counts.Add (c.suit, 1);
+				}
+			}
+			Value highestSeen = 0;
+			foreach (KeyValuePair<Suit, int> kvp in counts) {
+				if (kvp.Value >= 5) {
+					foreach(Card c in cards) {
+						if(c.suit.Equals(kvp.Key) && c.value > highestSeen) {
+							highestSeen = c.value;
+						}
+					}
+					return new Card(kvp.Key, highestSeen);
+				}
+			}
+
+			return null;
+		}
+
+
 		/* Returns the highest card in the straight,
 		 * or an empty string if there is no straight */
 		public String HasStraight()
@@ -87,27 +176,15 @@ namespace TexasHoldemHandEvaluator
 			return highestSeen.ToString ();
 		}
 
-		/* Returns the Card value of the quads on the board, or 
-		 * an empty string if there are no quads */
-		public String HasQuads()
+		public String HasPair()
 		{
-			var counts = new Dictionary<Value, int>();
-			foreach(Card c in cards) {
-				if(counts.ContainsKey(c.value)) {
-					counts[c.value] += 1;
-				} else {
-					counts.Add(c.value, 1);
-				}
-			}
-			foreach(KeyValuePair<Value, int> kvp in counts) {
-				if(kvp.Value == 4) {
-					return kvp.Key.ToString();
-				}
-			}
-
 			return "";
 		}
 
+		public String HasHighCard()
+		{
+			return "";
+		}
 	}
 }
 
--- NUnitTests.cs
+++ NUnitTests.cs
@@ -3,7 +3,202 @@ using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace TexasHoldemHandEvaluator
-{
+{	
+	[TestFixture()]
+	public class NUnitTestHasStraightFlush
+	{
+		[Test()]
+		public void TestHasStraightFlushHearts ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Queen));
+			c.Add (new Card(Suit.Hearts, Value.Ten));
+			c.Add (new Card(Suit.Hearts, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.King));
+			hc.Add (new Card(Suit.Spades, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual (new Card(Suit.Hearts, Value.Ace), test.HasStraightFlush());
+		}
+
+		[Test()]
+		public void TestHasStraightFlushFail ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Nine));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			hc.Add (new Card(Suit.Clubs, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual (null, test.HasStraightFlush());
+		}
+
+		[Test()]
+		public void TestHasStraightFlushSpades ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Spades, Value.Ace));
+			c.Add (new Card(Suit.Spades, Value.Queen));
+			c.Add (new Card(Suit.Spades, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Spades, Value.King));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual (new Card(Suit.Spades, Value.Ace), test.HasStraightFlush());
+		}
+
+		[Test()]
+		public void TestHasStraightFlushSpadesLow ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Spades, Value.Nine));
+			c.Add (new Card(Suit.Spades, Value.Queen));
+			c.Add (new Card(Suit.Spades, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Spades, Value.King));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual (new Card(Suit.Spades, Value.King), test.HasStraightFlush());
+		}
+
+		[Test()]
+		public void TestHasStraightFlushWheel ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Spades, Value.Two));
+			c.Add (new Card(Suit.Spades, Value.Three));
+			c.Add (new Card(Suit.Spades, Value.Five));
+			c.Add (new Card(Suit.Spades, Value.Four));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Spades, Value.Ace));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual (new Card(Suit.Spades, Value.Five), test.HasStraightFlush());
+		}
+	}
+
+	[TestFixture()]
+	public class NUnitTestHasQuads
+	{
+		[Test()]
+		public void TestHasQuadsNormal ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Nine));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Ten));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			hc.Add (new Card(Suit.Clubs, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("Ten", test.HasQuads());
+		}
+
+		[Test()]
+		public void TestHasQuadsTrips ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Nine));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			hc.Add (new Card(Suit.Clubs, Value.Ten));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("", test.HasQuads());
+		}
+
+		[Test()]
+		public void TestHasQuadsNoPairs ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Nine));
+			c.Add (new Card(Suit.Diamonds, Value.King));
+			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			hc.Add (new Card(Suit.Clubs, Value.Three));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("", test.HasQuads());
+		}
+	}
+
+	
+	[TestFixture()]
+	public class NUnitTestHasFullHouse
+	{
+	}
+
+	[TestFixture()]
+	public class NUnitTestHasFlush
+	{
+		[Test]
+		public void TestHasFlush ()
+		{
+			var c = new List<Card> ();
+			var hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Ten));
+			c.Add (new Card(Suit.Diamonds, Value.King));
+			c.Add (new Card(Suit.Hearts, Value.Jack));
+			c.Add (new Card(Suit.Hearts, Value.Queen));
+			hc.Add (new Card(Suit.Clubs, Value.Seven));
+			hc.Add (new Card(Suit.Hearts, Value.Six));
+			var test = new Hand (c, hc);
+			Assert.AreEqual (new Card(Suit.Hearts, Value.Ace), test.HasFlush());
+		}
+
+		[Test]
+		public void TestHasFlushSixCard ()
+		{
+			var c = new List<Card> ();
+			var hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Three));
+			c.Add (new Card(Suit.Hearts, Value.Ten));
+			c.Add (new Card(Suit.Diamonds, Value.King));
+			c.Add (new Card(Suit.Hearts, Value.Jack));
+			c.Add (new Card(Suit.Hearts, Value.Queen));
+			hc.Add (new Card(Suit.Hearts, Value.Seven));
+			hc.Add (new Card(Suit.Hearts, Value.Six));
+			var test = new Hand (c, hc);
+			Assert.AreEqual(new Card(Suit.Hearts, Value.Queen), test.HasFlush());
+		}
+
+		[Test]
+		public void TestHasFlushFourCard ()
+		{
+			var c = new List<Card> ();
+			var hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Ten));
+			c.Add (new Card(Suit.Diamonds, Value.King));
+			c.Add (new Card(Suit.Hearts, Value.Jack));
+			c.Add (new Card(Suit.Hearts, Value.Queen));
+			hc.Add (new Card(Suit.Clubs, Value.Seven));
+			hc.Add (new Card(Suit.Clubs, Value.Six));
+			var test = new Hand (c, hc);
+			Assert.AreEqual (null, test.HasFlush());
+		}
+
+	}
+
 	[TestFixture]
 	public class NUnitTestHasStraight
 	{
@@ -22,7 +217,7 @@ namespace TexasHoldemHandEvaluator
 			var test = new Hand (c, hc);
 			Assert.AreEqual ("Ace", test.HasStraight());
 		}
-		
+
 		[Test]
 		public void TestHasStraightLastCards ()
 		{
@@ -38,7 +233,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("Ten", test.HasStraight());
 		}
-		
+
 		[Test()]
 		public void TestHasStraightWheel ()
 		{
@@ -70,7 +265,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("Queen", test.HasStraight());
 		}
-			
+
 		[Test()]
 		public void TestHasStraightGap ()
 		{
@@ -86,7 +281,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("Jack", test.HasStraight());
 		}
-		
+
 		[Test()]
 		public void TestHasStraightFourCard ()
 		{
@@ -102,7 +297,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("", test.HasStraight());
 		}
-		
+
 		[Test()]
 		public void TestHasStraightFourCardWheel ()
 		{
@@ -118,7 +313,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("", test.HasStraight());
 		}
-		
+
 		[Test()]
 		public void TestHasStraightThreeCard ()
 		{
@@ -186,7 +381,7 @@ namespace TexasHoldemHandEvaluator
 			Hand test = new Hand (c, hc);
 			Assert.AreEqual ("", test.HasTrips());
 		}
-		
+
 		[Test()]
 		public void TestHasTripsNoPairs ()
 		{
@@ -205,55 +400,106 @@ namespace TexasHoldemHandEvaluator
 	}
 	
 	[TestFixture()]
-	public class NUnitTestHasQuads
+	public class NUnitTestHasPair
 	{
 		[Test()]
-		public void TestHasQuadsNormal ()
+		public void TestHasPairAces ()
 		{
 			List<Card> c = new List<Card> ();
 			List<Card> hc = new List<Card> ();
 			c.Add (new Card(Suit.Hearts, Value.Ace));
 			c.Add (new Card(Suit.Hearts, Value.Nine));
 			c.Add (new Card(Suit.Diamonds, Value.Ten));
-			c.Add (new Card(Suit.Spades, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.King));
 			c.Add (new Card(Suit.Clubs, Value.Two));
-			hc.Add (new Card(Suit.Hearts, Value.Ten));
-			hc.Add (new Card(Suit.Clubs, Value.Ten));
+			hc.Add (new Card(Suit.Hearts, Value.Three));
+			hc.Add (new Card(Suit.Clubs, Value.Ace));
 			Hand test = new Hand (c, hc);
-			String s = test.HasQuads();
-			Assert.AreEqual ("Ten", s);
+			Assert.AreEqual ("Ace", test.HasPair());
 		}
 
 		[Test()]
-		public void TestHasQuadsTrips ()
+		public void TestHasPairThrees ()
 		{
 			List<Card> c = new List<Card> ();
 			List<Card> hc = new List<Card> ();
-			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Three));
 			c.Add (new Card(Suit.Hearts, Value.Nine));
 			c.Add (new Card(Suit.Diamonds, Value.Ten));
 			c.Add (new Card(Suit.Spades, Value.Jack));
 			c.Add (new Card(Suit.Clubs, Value.Two));
-			hc.Add (new Card(Suit.Hearts, Value.Ten));
-			hc.Add (new Card(Suit.Clubs, Value.Ten));
+			hc.Add (new Card(Suit.Hearts, Value.Three));
+			hc.Add (new Card(Suit.Clubs, Value.Five));
 			Hand test = new Hand (c, hc);
-			Assert.AreEqual ("", test.HasQuads());
+			Assert.AreEqual ("Three", test.HasPair());
 		}
 
 		[Test()]
-		public void TestHasQuadsNoPairs ()
+		public void TestHasPairFail ()
 		{
 			List<Card> c = new List<Card> ();
 			List<Card> hc = new List<Card> ();
 			c.Add (new Card(Suit.Hearts, Value.Ace));
-			c.Add (new Card(Suit.Hearts, Value.Nine));
-			c.Add (new Card(Suit.Diamonds, Value.King));
-			c.Add (new Card(Suit.Spades, Value.Jack));
+			c.Add (new Card(Suit.Hearts, Value.Three));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Five));
 			c.Add (new Card(Suit.Clubs, Value.Two));
-			hc.Add (new Card(Suit.Hearts, Value.Ten));
+			hc.Add (new Card(Suit.Hearts, Value.Eight));
+			hc.Add (new Card(Suit.Clubs, Value.King));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("", test.HasPair());
+		}
+	}
+	
+	[TestFixture()]
+	public class NUnitTestHasHighCard
+	{
+		[Test()]
+		public void TestHasHighCardAce ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Ace));
+			c.Add (new Card(Suit.Hearts, Value.Three));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Five));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Eight));
+			hc.Add (new Card(Suit.Clubs, Value.King));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("Ace", test.HasHighCard());
+		}
+
+		[Test()]
+		public void TestHasHighCardKing ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Seven));
+			c.Add (new Card(Suit.Hearts, Value.Three));
+			c.Add (new Card(Suit.Diamonds, Value.Ten));
+			c.Add (new Card(Suit.Spades, Value.Five));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Eight));
+			hc.Add (new Card(Suit.Clubs, Value.King));
+			Hand test = new Hand (c, hc);
+			Assert.AreEqual ("King", test.HasHighCard());
+		}
+
+		[Test()]
+		public void TestHasHighCardLow ()
+		{
+			List<Card> c = new List<Card> ();
+			List<Card> hc = new List<Card> ();
+			c.Add (new Card(Suit.Hearts, Value.Nine));
+			c.Add (new Card(Suit.Hearts, Value.Seven));
+			c.Add (new Card(Suit.Diamonds, Value.Five));
+			c.Add (new Card(Suit.Spades, Value.Eight));
+			c.Add (new Card(Suit.Clubs, Value.Two));
+			hc.Add (new Card(Suit.Hearts, Value.Four));
 			hc.Add (new Card(Suit.Clubs, Value.Three));
 			Hand test = new Hand (c, hc);
-			Assert.AreEqual ("", test.HasQuads());
+			Assert.AreEqual ("", test.HasHighCard());
 		}
 	}
 }
